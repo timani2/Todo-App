99840c584a2b707666e5fa3f18f88dfea0e8f981
timani2 <tanphong0730@gmail.com>
2025-10-08 09:21:01 +0700

    create project

---
+++ src/main/java/ojt/se/fu/vn/modle/Task.java
@@ -0,0 +1,58 @@
+package ojt.se.fu.vn.modle;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class Task implements Serializable {
+    private int id;
+    private String title;
+    private String status; // "INCOMPLETE" or "COMPLETED"
+    private LocalDate createdAt;
+
+    public Task() {
+    }
+
+    public Task(int id, String title, String status, LocalDate createdAt) {
+        this.id = id;
+        this.title = title;
+        this.status = status;
+        this.createdAt = createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Task [id=" + id + ", title=" + title + ", status=" + status + ", createdAt=" + createdAt + "]";
+    }
+}
