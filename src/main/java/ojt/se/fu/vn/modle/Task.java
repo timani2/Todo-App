d4baa855b9c8d742d700bd31ecb3b44bf01ab09c
timani2 <tanphong0730@gmail.com>
2025-10-20 09:19:56 +0700

    update class task(Add new attribute "userId")

--- src/main/java/ojt/se/fu/vn/modle/Task.java
+++ src/main/java/ojt/se/fu/vn/modle/Task.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 
 public class Task implements Serializable {
     private int id;
+    private int userId;
     private String title;
     private String status; // "INCOMPLETE" or "COMPLETED"
     private LocalDate createdAt;
@@ -12,8 +13,9 @@ public class Task implements Serializable {
     public Task() {
     }
 
-    public Task(int id, String title, String status, LocalDate createdAt) {
+    public Task(int id, int userId, String title, String status, LocalDate createdAt) {
         this.id = id;
+        this.userId = userId;
         this.title = title;
         this.status = status;
         this.createdAt = createdAt;
@@ -25,6 +27,14 @@ public class Task implements Serializable {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
     }
 
     public String getTitle() {
